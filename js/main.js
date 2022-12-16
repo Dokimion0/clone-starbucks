b0a4f366b478e355c1818c225af288382572f7b8
Dokimion0 <leecy3610@gmail.com>
2022-12-16 16:55:34 +0900

    refactor : Use arrow function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-console.log("js");
+console.log('js');
 
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-const searchIconEl = searchEl.querySelector("span");
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+const searchIconEl = searchEl.querySelector('span');
 let buttonClick = false;
 
-searchEl.addEventListener("click", function () {
-  searchInputEl.style.width = "180px";
-  searchInputEl.setAttribute("placeholder", "통합검색");
+searchEl.addEventListener('click', () => {
+  searchInputEl.style.width = '180px';
+  searchInputEl.setAttribute('placeholder', '통합검색');
   buttonClick = true;
 });
 
-searchIconEl.addEventListener("click", function () {
-  if (buttonClick === true && searchInputEl.value === "") {
-    alert("검색어를 입력하세요.");
-  } else if (buttonClick === true && searchInputEl.value !== "") {
-    searchInputEl.style.width = "34px";
+searchIconEl.addEventListener('click', () => {
+  if (buttonClick === true && searchInputEl.value === '') {
+    alert('검색어를 입력하세요.');
+  } else if (buttonClick === true && searchInputEl.value !== '') {
     buttonClick = false;
   }
   console.log(buttonClick);
